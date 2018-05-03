2508cab1f17817b80151d2cb59be5b95a4b1ac93
jqngarcia <35226532+jqngarcia@users.noreply.github.com>
2018-05-03 21:14:19 +0200

    Update pois-type.js

--- docs/assets/js/pois-type.js
+++ docs/assets/js/pois-type.js
@@ -6,4 +6,9 @@ var pois = {
 	query: '[shop=car_repair][member_of=ACI]',
 	iconName: 'repair'
     },
+    wheel_repair: {
+	name: 'Recambis',
+	query: '[shop=car_parts][member_of=ACI]',
+	iconName: 'tires'
+    },
 }
