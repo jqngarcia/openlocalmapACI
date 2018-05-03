2d831a6afe79b5c07ffc586ed1dced224a2391f1
jqngarcia <35226532+jqngarcia@users.noreply.github.com>
2018-05-03 21:17:33 +0200

    Update pois-type.js

--- docs/assets/js/pois-type.js
+++ docs/assets/js/pois-type.js
@@ -1,11 +1,19 @@
 // All the POIs shown in the map
 
 var pois = {
+
+    bar: {
+	name: 'Bar',
+	query: '[amenity=bar][member_of=ACI]',
+	iconName: 'bar_coktail'
+    },
+	
     repair: {
 	name: 'Mecànic',
 	query: '[shop=car_repair][member_of=ACI]',
 	iconName: 'repair'
     },
+    
     wheel_repair: {
 	name: 'Recambis',
 	query: '[shop=car_parts][member_of=ACI]',
