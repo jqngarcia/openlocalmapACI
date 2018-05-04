ae888cea7866694272ae274e6c991cd34c4f90b1
jqngarcia <35226532+jqngarcia@users.noreply.github.com>
2018-05-04 12:56:31 +0200

    Update pois-type.js

--- docs/assets/js/pois-type.js
+++ docs/assets/js/pois-type.js
@@ -1,22 +1,146 @@
-// All the POIs shown in the map
+    dentist: {
+	name: 'Dentista',
+	query: '[shop=dentist][member_of=ACI]',
+	iconName: 'dentist'
+    },
 
-var pois = {
+    veterinary: {
+	name: 'Veterinari',
+	query: '[amenity=veterinary][member_of=ACI]',
+	iconName: 'veterinary'
+    },
 
-    bar: {
-	name: 'Bar',
-	query: '[amenity=bar][member_of=ACI]',
-	iconName: 'bar_coktail'
+    hairdresser: {
+	name: 'Perruqueria',
+	query: '[shop=hairdresser][member_of=ACI]',
+	iconName: 'barber'
     },
-	
-    repair: {
-	name: 'Mecànic',
-	query: '[amenity=bar][member_of=ACI]',
-	iconName: 'repair'
+
+    beauty: {
+	name: 'Estètica',
+	query: '[shop=beauty][member_of=ACI]',
+	iconName: 'beautysalon'
     },
-    
-    wheel_repair: {
-	name: 'Recambis',
-	query: '[shop=car_parts][member_of=ACI]',
-	iconName: 'tires'
+
+    supermarket: {
+	name: 'Supermercat',
+	query: '[shop=supermarket][member_of=ACI]',
+	iconName: 'supermarket'
     },
+
+    greengrocer: {
+	name: 'Fruiteria',
+	query: '[shop=greengrocer][member_of=ACI]',
+	iconName: 'fruits'
+    },
+
+    herbalist: {
+	name: 'Herboristeria',
+	query: '[shop=herbalist][member_of=ACI]',
+	iconName: 'herbalist'
+    },
+
+    newsagent: {
+	name: 'Diaris',
+	query: '[office=newspaper][member_of=ACI]',
+	iconName: 'newsagent'
+    },
+
+    pet: {
+	name: 'Mascotes',
+	query: '[shop=pet][member_of=ACI]',
+	iconName: 'pets'
+    },
+
+    jewelry: {
+	name: 'Joieria',
+	query: '[shop=jewelry][member_of=ACI]',
+	iconName: 'jewelry'
+    },
+
+    fabric: {
+	name: 'Merceria',
+	query: '[shop=fabric][member_of=ACI]',
+	iconName: 'lingerie'
+    },
+
+    it: {
+	name: 'Informàtica',
+	query: '[office=it][member_of=ACI]',
+	iconName: 'computers'
+    },
+
+    butcher: {
+	name: 'Carniceria',
+	query: '[shop=butcher][member_of=ACI]',
+	iconName: 'butcher-2'
+    },
+
+    electrical_equipment: {
+	name: 'Mat.elèctric',
+	query: '[shop=electrical_equipment][member_of=ACI]',
+	iconName: 'power'
+    },
+
+    reforms: {
+	name: 'Construccions',
+	query: '[craft=reforms][member_of=ACI]',
+	iconName: 'plastering'
+    },
+
+    electrician: {
+	name: 'Electricista',
+	query: '[craft=electrician][member_of=ACI]',
+	iconName: 'power'
+    },
+
+    window_construction: {
+	name: 'Al·lumini',
+	query: '[craft=window_construction][member_of=ACI]',
+	iconName: 'glazer'
+    },
+
+    glaziery: {
+	name: 'Cristalleria',
+	query: '[shop=glaziery][member_of=ACI]',
+	iconName: 'glazer'
+    },
+
+    paint: {
+	name: 'Pintures',
+	query: '[shop=paint][member_of=ACI]',
+	iconName: 'paint'
+    },
+
+
+    furniture: {
+	name: 'Mobles',
+	query: '[shop=furniture][member_of=ACI]',
+	iconName: 'homecenter'
+    },
+
+    estate_agent: {
+	name: 'Immobiliària',
+	query: '[office=estate_agent]',
+	iconName: 'estateagent'
+    },
+
+    NGO: {
+	name: 'ONG i Fundacions',
+	query: '[amenity=ngo]',
+	iconName: 'communitycentre'
+    },
+
+    hotel: {
+	name: 'Hotel',
+	query: '[amenitym=hotel][member_of=ACI]',
+	iconName: 'hotel_0star',
+    },
+
+    sports_centre: {
+	name: 'Club',
+	query: '[leisure=sports_centre][member_of=ACI]',
+	iconName: 'stadium'
+    },
+
 }
